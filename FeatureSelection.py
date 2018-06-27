ac4954a63f40c4b9a0ebf3e1726a29073da6e308
tedydaniel <danielndelman@gmail.com>
2018-06-27 20:44:34 +0300

    FeatureSelection.py added

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -1,8 +1,35 @@
+import numpy as np
 
-BITS_FROM_END = 50
+def zerosInARow(vector, k):
+    vector = vector[-k:]
+    zeros = []
+    counter = 0
+    for i in vector:
+        if i == 0:
+            counter += 1
+        else:
+            zeros.append(counter)
+            counter = 0
+    return np.mean(np.asarray(zeros))
 
-def zerosInARow(vector):
-    return
 
-def onesInARow(vector):
-    return
+def onesInARow(vector, k):
+    vector = vector[-k:]
+    ones = []
+    counter = 0
+    for i in vector:
+        if i == 1:
+            counter += 1
+        else:
+            ones.append(counter)
+            counter = 0
+    return np.mean(np.asarray(ones))
+
+def numOfAlternations(vector, k):
+    vector = vector[-k:]
+    counter = 0
+    for i in range(k - 1):
+        if vector[i] != vector[i + 1]:
+            counter += 1
+    return counter
+

97b9a415256dd151594e6ec09a9aebee1d460224
elazar.cohen1 <aueuks>
2018-06-27 20:33:50 +0300

    s

---
+++ learner.py
@@ -0,0 +1,17 @@
+class Learner:
+
+    def __init__(self):
+        pass
+
+    def learn(self, X):
+        pass
+
+    def predict(self, X, k):
+        """
+        :param X: list of sequences
+        :param k: k next bits
+        """
+        pass
+
+    def predict_seq(self, seq, k):
+        pass

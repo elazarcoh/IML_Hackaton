39f3761f647b7a125e9a616395a85fe363c8c576
tedydaniel <danielndelman@gmail.com>
2018-06-28 14:15:17 +0300

    FeatureSelection.py updated

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -1,4 +1,8 @@
 import numpy as np
+import perceptron as pt
+import sklearn.svm as svm
+
+
 
 def zerosInARow(vector, k):
     if len(vector) < k:
@@ -12,7 +16,7 @@ def zerosInARow(vector, k):
         else:
             zeros.append(counter)
             counter = 0
-    return np.mean(np.asarray(zeros))
+    return np.mean(np.asarray(zeros), dtype=np.int)
 
 
 def onesInARow(vector, k):
@@ -27,7 +31,7 @@ def onesInARow(vector, k):
         else:
             ones.append(counter)
             counter = 0
-    return np.mean(np.asarray(ones))
+    return np.mean(np.asarray(ones), dtype=np.int)
 
 
 def numOfAlternations(vector, k):
@@ -75,10 +79,60 @@ def getFeatures(vector, k):
     features = []
     features.append(zerosInARow(vector, k))
     features.append(onesInARow(vector, k))
-    features.append(numOfAlternations(vector, k))
-    features.append(numberOfOnes(vector, k)[0][1])
-    features.append(numOfIncreasingSeqs(vector, k))
+    # features.append(numOfAlternations(vector, k))
+    # features.append(numberOfOnes(vector, k)[0][1])
+    # features.append(numOfIncreasingSeqs(vector, k))
     return features
+
+
+file = open("human.txt", "r")
+vectors = []
+line = file.readline()
+while line:
+    vectors.append(line)
+    line = file.readline()
+
+finalVectors = []
+for line in vectors:
+    vector = []
+    for letter in line:
+        if letter != "\n":
+            vector.append(int(letter))
+    finalVectors.append(vector)
+
+# finalVectors = np.asarray(finalVectors)
+# forTrain = np.random.random_integers(0, len(finalVectors), len(finalVectors) * 0.9)
+
+forTrain = finalVectors[:1000]
+forTest = finalVectors[1000:]
+
+trainX = []
+trainY = []
+testX = []
+testY = []
+
+for vector in forTrain:
+    trainX.append(vector[-50: -1])
+    trainY.append(vector[-1:][0])
+
+for vector in forTest:
+    testX.append(vector[-50: -1])
+    testY.append(vector[-1:][0])
+
+X_train = []
+X_test = []
+for train in trainX:
+    X_train.append(getFeatures(train, 40))
+for test in testX:
+    X_test.append(getFeatures(test, 40))
+
+# ptr = pt.Perceptron()
+X_train = np.asarray(X_train)
+trainY = np.asarray(trainY)
+svc = svm.SVC(C=1e10, kernel='linear')
+print(X_train)
+svc.fit(X_train, trainY)
+
 
 
 

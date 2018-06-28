862535368a8d8d208973a0be30402ef412c095ef
elazar.cohen1 <aueuks>
2018-06-28 15:23:23 +0300

    s

--- a.py
+++ a.py
@@ -3,6 +3,9 @@ import learner
 import matplotlib.pyplot as plt
 import editdistance as ed
 
+from keras.layers import SimpleRNN, TimeDistributed, Dense
+from keras.models import Sequential
+
 
 def prepare_data(data):
     pass
@@ -10,7 +13,6 @@ def prepare_data(data):
 
 d = np.loadtxt('human.txt', dtype=str)
 A = []
-B = []
 # m = {'0': 0, '1': 1}
 for x in d:
     if len(x) > 70:
@@ -19,61 +21,72 @@ for x in d:
 A = np.array(A)
 
 
-def draw(data, test_size,l):
+def draw(data, test_size, xl, yl):
     np.random.shuffle(data)
     # train_data = data[test_size:, :-20]
     # train_labels = data[test_size:, -20:]
     # test_data = data[:test_size, :-20]
     # test_labels = data[:test_size, -20:]
+
     train_data = data[test_size:]
     train_labels = data[test_size:]
-    train_data = [s[-l:-1] for s in train_data]
-    train_labels = [int(s[-1]) for s in train_labels]
+    train_data = np.array([list(map(int, s[-xl - yl:-yl])) for s in train_data])
+    train_labels = np.array([list(map(int, s[-yl:])) for s in train_labels])
 
     test_data = data[:test_size]
     test_labels = data[:test_size]
-    test_data = [s[-l:-1] for s in test_data]
-    r = [int(s[-1]) for s in test_labels]
-    r = np.array(r, dtype=int)
+    test_data = np.array([list(map(int, s[-xl - yl:-yl])) for s in test_data])
+    test_labels = np.array([list(map(int, s[-yl:])) for s in test_labels])
 
-    return (np.array(train_data), np.array(train_labels)), (
-        np.array(test_data), r)
+    # return (train_data, train_labels), (test_data, test_labels)
+    return (np.expand_dims(train_data, axis=2), np.expand_dims(train_labels, axis=2)), \
+           (np.expand_dims(test_data, axis=2), np.expand_dims(test_labels, axis=2))
 
+# def knn_procedure(k, test_size,l):
+#     train_data, test_data = draw(A, test_size,)
+#     train_data, train_labels = train_data
+#     test_data, test_labels = test_data
+#     p = learner.knn(k)
+#     p.fit(train_data, train_labels)
+#     return p.score(test_data, test_labels)
 
-def knn_procedure(k, test_size,l):
-    train_data, test_data = draw(A, test_size,l)
-    train_data, train_labels = train_data
-    test_data, test_labels = test_data
-    p = learner.knn(k)
-    p.fit(train_data, train_labels)
-    return p.score(test_data, test_labels)
-
-
-K = range(1,50,2)
+K = range(1, 50, 2)
 repeat = 10
 knn_res = []
 test_size = 100
 
-for k in K:
-    res = sum(knn_procedure(45, test_size,k) for _ in range(repeat)) / repeat
-    knn_res.append(res)
+# for k in K:
+#     res = sum(knn_procedure(45, test_size,k) for _ in range(repeat)) / repeat
+#     knn_res.append(res)
 
 # print(np.mean(list(map(str.__len__,A))))
 # plt.hist(list(map(str.__len__,A)),80)
 # plt.show()
 
-plt.figure(0)
-plt.title("K nearest accuracy ratio")
-plt.xticks(K)
-plt.xlabel('m')
-plt.ylabel('accuracy ratio')
+# plt.figure(0)
+# plt.title("K nearest accuracy ratio")
+# plt.xticks(K)
+# plt.xlabel('m')
+# plt.ylabel('accuracy ratio')
+#
+# plt.plot(K, knn_res)
+# plt.show()
 
-plt.plot(K, knn_res)
-plt.show()
+train_data, test_data = draw(A, test_size=100, xl=50, yl=20)
+train_data, train_labels = train_data
+test_data, test_labels = test_data
 
-# train_data, test_data = draw(A, 100)
-# train_data, train_labels = train_data
-# test_data, test_labels = test_data
 # p = learner.knn(30)
 # p.fit(train_data, train_labels)
 
+
+def RNN(x, y):
+    mod = Sequential()
+    mod.add(SimpleRNN(input_dim=50, output_dim=20, return_sequences=True))
+    # mod.add(TimeDistributed(Dense(output_dim=1, activation="sigmoid")))
+    mod.compile(loss="mse", optimizer="Adam")
+    mod.fit(x, y, batch_size=20)
+    # print(mod.predict(np.array([[[1], [0], [0], [0], [0], [0]]])))
+
+
+RNN(train_data, train_labels)
